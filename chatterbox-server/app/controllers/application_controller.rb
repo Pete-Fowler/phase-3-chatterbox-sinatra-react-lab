c98a0356568aaeedcc16a2819fb42c3d4af10d25
Pete Fowler <fowler.pete@gmail.com>
2022-09-29 11:29:35 -0600

    Add createget, post, patch

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,22 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  get '/messages' do 
+    Message.all.order(:created_at).to_json 
+  end 
+
+  post '/messages' do 
+    msg = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    msg.to_json
+  end 
+
+  patch '/messages/:id' do
+    msg = Message.find(params[:id]) 
+    msg.update(body: params[:body])
+    msg.to_json 
+  end 
+
 end
